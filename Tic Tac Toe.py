88d84bde9ab89055f4e6c7f580319b7b716bc29f
hariharan1307 <67898468+hariharan1307@users.noreply.github.com>
2020-07-11 13:40:27 +0530

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,47 +1,58 @@
-Board={
-    1: " ",2:" ",3: " ",
-    4: " ",5:" ",6: " ",
-    7: " ",8:" ",9: " "
+Board = {
+    1: " ", 2: " ", 3: " ",
+    4: " ", 5: " ", 6: " ",
+    7: " ", 8: " ", 9: " "
 
 }
-player=1
-playerOne=input("Enter the first player name: ")
-playerTwo=input("Enter the second player name: ")
+
+player = 1
+playerOne = input("Enter the first player name: ")
+playerTwo = input("Enter the second player name: ")
+
+totalMoves = 0
+End = 0
 
 
-
-totalMoves=0
-End=0
 def GameOver():
     # Horizontal Conditions for player one
-    if  ( Board[1]=="x" and Board[2]=="x" and Board[3]=="x" ) or (Board[4]=="x" and Board[5]=="x" and Board[6]=="x") or (Board[7]=="x" and Board[8]=="x" and Board[9]=="x"):
-        print(playerOne," has won")
-        End=1
-        return End
-    # Vertical condtions
-    elif (Board[1]=="x" and Board[4]=="x" and Board[7]=="x") or (Board[2]=="x" and Board[5]=="x" and Board[8]=="x") or (Board[3]=="x" and Board[6]=="x" and Board[9]=="x"):
-        print(playerOne," has won")
+    if (Board[1] == "x" and Board[2] == "x" and Board[3] == "x") or (
+            Board[4] == "x" and Board[5] == "x" and Board[6] == "x") or (
+            Board[7] == "x" and Board[8] == "x" and Board[9] == "x"):
+        print(playerOne, " has won")
         End = 1
         return End
-    #Diagonal conditions
-    elif (Board[1]=="x" and Board[5]=="x" and Board[9]=="x") or (Board[3]=="x" and Board[5]=="x" and Board[7]=="x"):
-        print(playerOne," has won")
+    # Vertical condtions
+    elif (Board[1] == "x" and Board[4] == "x" and Board[7] == "x") or (
+            Board[2] == "x" and Board[5] == "x" and Board[8] == "x") or (
+            Board[3] == "x" and Board[6] == "x" and Board[9] == "x"):
+        print(playerOne, " has won")
+        End = 1
+        return End
+    # Diagonal conditions
+    elif (Board[1] == "x" and Board[5] == "x" and Board[9] == "x") or (
+            Board[3] == "x" and Board[5] == "x" and Board[7] == "x"):
+        print(playerOne, " has won")
         End = 1
         return End
     # Horizontal Conditions for player two
 
-    elif ( Board[1]=="o" and Board[2]=="o" and Board[3]=="o" ) or (Board[4]=="o" and Board[5]=="o" and Board[6]=="o") or (Board[7]=="o" and Board[8]=="o" and Board[9]=="o"):
-        print(playerTwo," has won")
-        End=1
-        return End
-    # Vertical condtions
-    elif (Board[1]=="o" and Board[4]=="o" and Board[7]=="o") or (Board[2]=="o" and Board[5]=="o" and Board[8]=="o") or (Board[3]=="o" and Board[6]=="o" and Board[9]=="o"):
-        print(playerTwo," has won")
+    elif (Board[1] == "o" and Board[2] == "o" and Board[3] == "o") or (
+            Board[4] == "o" and Board[5] == "o" and Board[6] == "o") or (
+            Board[7] == "o" and Board[8] == "o" and Board[9] == "o"):
+        print(playerTwo, " has won")
         End = 1
         return End
-    #Diagonal conditions
-    elif (Board[1]=="o" and Board[5]=="o" and Board[9]=="o") or (Board[3]=="o" and Board[5]=="o" and Board[7]=="o"):
-        print(playerTwo," has won")
+    # Vertical condtions
+    elif (Board[1] == "o" and Board[4] == "o" and Board[7] == "o") or (
+            Board[2] == "o" and Board[5] == "o" and Board[8] == "o") or (
+            Board[3] == "o" and Board[6] == "o" and Board[9] == "o"):
+        print(playerTwo, " has won")
+        End = 1
+        return End
+    # Diagonal conditions
+    elif (Board[1] == "o" and Board[5] == "o" and Board[9] == "o") or (
+            Board[3] == "o" and Board[5] == "o" and Board[7] == "o"):
+        print(playerTwo, " has won")
         End = 1
         return End
     return 0
@@ -56,26 +67,24 @@ print("0 |0 |x")
 print("------------------------------------")
 
 while True:
-    print(Board[1] + " |" + Board[2] + " | " + Board[3])
+    print(Board[1] + " |" + Board[2] + " |" + Board[3])
     print("--+--+--")
     print(Board[4] + " |" + Board[5] + " |" + Board[6])
     print("--+--+--")
     print(Board[7] + " |" + Board[8] + " |" + Board[9])
-       #End=GameOver()
+    End = GameOver()
     if totalMoves == 9:
         print("Match draw")
         break
-    elif End==1:
-        GameOver()
+    elif End == 1:
         break
+
     while True:
-        if player==1:
-            inputOne=int(input("First player "))
-            if inputOne in Board.keys() and Board[inputOne]== " ":
-                Board[inputOne]="x"
-                End=GameOver()
-                #print(GameOver())
-                player=2
+        if player == 1:
+            inputOne = int(input("First player "))
+            if inputOne in Board.keys() and Board[inputOne] == " ":
+                Board[inputOne] = "x"
+                player = 2
                 break
             else:
                 print("Wrong input")
@@ -84,13 +93,11 @@ while True:
             inputTwo = int(input("Second player:"))
             if inputTwo in Board.keys() and Board[inputTwo] == " ":
                 Board[inputTwo] = "o"
-                End=GameOver()
-                #print(GameOver())
                 player = 1
                 break
             else:
                 print("Wrong input")
                 continue
-    totalMoves+=1
+    totalMoves += 1
     print("---------------------------------------------------------")
     print()
