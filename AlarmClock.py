cea554e4b3d10f2a236b672feb4197a47fc81a0e
hariharan1307 <67898468+hariharan1307@users.noreply.github.com>
2020-07-11 13:05:56 +0530

    Update AlarmClock.py

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,32 +1,32 @@
 import datetime
-print("Required format : hr:min:sec--> 07:20:45")
-WakeupTimw = input("WakeUp time? :")
-WakeupList = WakeupTimw.split(":")
-hour = int(WakeupList[0])
-minutes = int(WakeupList[1])
+print("Required format : hr:min am/pm--> 07:20 am")
+inputTime = input("WakeUp time? :")
+WakeupList = inputTime.split(" ")
+DayOrNight=WakeupList[1]
+Time = WakeupList[0]
+WakeupTime=Time.split(":")
+hour=int(WakeupTime[0])
+minutes = int(WakeupTime[1])
+
+if DayOrNight=="pm":
+    hour+=12
 
 
 def validation(hr,min):
     if hr > 24:
-        print("Hours should not exceed 24")
+        if min > 60:
+            print("hours and minutes should not exceed the standard time limits")
+            return False
+        print("hours should not exceed 24")
         return False
-    if min > 60:
-        print("Minutes should not exceed 24")
-        return False
+
+    print("waiting for the alarm ", str(hour).zfill(2), ":", str(minutes).zfill(2),DayOrNight)
 
     return True
 
-print("waiting for the alarm ", hour, ":", minutes)
 
-
-while True:
-    if validation(hour,minutes):
-        if (hour == datetime.datetime.now().hour and minutes == datetime.datetime.now().min):
-            print("Time to wakeup kid!!!")
+if validation(hour,minutes):
+    while True:
+        if hour==datetime.datetime.now().hour and minutes== datetime.datetime.now().minute:
+            print("its ",str(hour).zfill(2),":",str(minutes).zfill(2),DayOrNight, " time to wakeup kid!!!!")
             break
-
-
-
-
-
-
